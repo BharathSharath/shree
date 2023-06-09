7284671d363f247a8a8fe89db84397a744696c1c
BharathSharath <103111105+BharathSharath@users.noreply.github.com>
2023-06-09 13:24:18 +0530

    Update Project.cs

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,6 +1,10 @@
-public class Project
+namespace YourApplication.Models
 {
-    public string ProjectID { get; set; }
-    public string ProjectName { get; set; }
-    public int MemberID { get; set; }
+    public class Project
+    {
+        public string ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public int KYCID { get; set; }
+        public KYC KYC { get; set; }
+    }
 }
